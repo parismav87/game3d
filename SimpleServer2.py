2a24152694856e748b1191f4e15f9ac715611c89
Vmylonas <vmylonas@phed.auth.gr>
2022-03-16 00:02:17 +0200

    Changes 15-03-2022

--- SimpleServer2.py
+++ SimpleServer2.py
@@ -19,43 +19,68 @@ timeout = 3000
 myConnection = cManager.openTCPClientConnection(ip_address, port_address, timeout)
 if myConnection:
     cReader.addConnection(myConnection)
-
-
 socketBuffer = ''
 timestamp=[]
+
+
 def SocketListener():
     global socketBuffer
     global timestamp
     while True:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(('localhost', 8089))
+                s.bind(('localhost', 1234))
+
                 s.listen()
                 currenttime=time.time()
+
                 while True:
+
                     conn, addr = s.accept()
+
                     with conn:
                         while True:
                             data = conn.recv(1024)
+                            #print(data)
                             if len(data) != 0:
-                                socketBuffer += data.decode('utf-8')
+                                socketBuffer += data.decode('utf-16')
                                 currenttime2=time.time()
                                 timestamp.append(currenttime2-currenttime)
                                 currenttime=currenttime2
+                                #print((socketBuffer))
+
+
                             else: break
-        except: break
+        except: pass
 threadSocket = Thread(target = SocketListener)
 threadSocket.start()
+
 while True:
-    if '\t' in socketBuffer:
-        tempbuffer=socketBuffer.rsplit('\t',1)
-        socketBuffer = tempbuffer[1]
-        measurements=tempbuffer[0].split('\t')
-        for i in measurements:
-            items=i.split(';')
-            print(items)
-            pkg = NetDatagram()
-            pkg.addString(i)
-            cWriter.send(pkg, myConnection)
-                # data = s.recv(1024)
-                # s.close() 
+
+    last_pkg = socketBuffer.split('ENDSTART')[-1]
+
+    if ('START' in last_pkg):
+        last_pkg=last_pkg.replace('START','')
+
+    #last_pkg.replace('END','')
+    print(last_pkg)
+
+    if 'END' in last_pkg:
+        print(last_pkg)
+        CoPX = last_pkg.split()[1]
+
+        CoPY = last_pkg.split()[2]
+
+
+
+        print(CoPX, CoPY)
+
+
+        pkg = NetDatagram()
+        pkg.addString(CoPX+';'+CoPY)
+
+        cWriter.send(pkg, myConnection)
+        time.sleep(1/20)
+
+
+
--- game.py
+++ game.py
@@ -2,7 +2,8 @@ from direct.showbase.ShowBase import ShowBase
 from math import pi, sin, cos
 from direct.task import Task
 from direct.actor.Actor import Actor
-from panda3d.core import Thread, DirectionalLight, AmbientLight, loadPrcFileData, CollisionNode, CollisionCapsule, CollisionSphere, CollisionTraverser, CollisionHandlerEvent, CollisionHandlerQueue, CollisionHandlerPusher, TextNode
+from panda3d.core import Thread, DirectionalLight, AmbientLight, loadPrcFileData, CollisionNode, CollisionCapsule, \
+    CollisionSphere, CollisionTraverser, CollisionHandlerEvent, CollisionHandlerQueue, CollisionHandlerPusher, TextNode
 from plane import *
 import random
 from panda3d.core import QueuedConnectionManager
@@ -16,14 +17,15 @@ from direct.distributed.PyDatagramIterator import PyDatagramIterator
 import datetime
 import re
 
-
-loadPrcFileData('', 'win-size 1280 800') 
+loadPrcFileData('', 'win-size 1280 800')
 
 
 class MyApp(ShowBase):
 
     def __init__(self, numObstacles):
-
+        self.collecting = False
+        self.baselineX = 0
+        self.baselineY = 0
         self.numObstacles = numObstacles
         self.hoops = []
         self.camX = 0
@@ -41,16 +43,13 @@ class MyApp(ShowBase):
         self.cReader = QueuedConnectionReader(self.cManager, 0)
         self.cWriter = ConnectionWriter(self.cManager, 0)
         self.activeConnections = []
-        self.port_address = 5000 #No-other TCP/IP services are using this port
-        self.backlog = 1000 #If we ignore 1,000 connection attempts, something is wrong!
+        self.port_address = 5000  # No-other TCP/IP services are using this port
+        self.backlog = 1000  # If we ignore 1,000 connection attempts, something is wrong!
         self.tcpSocket = self.cManager.openTCPServerRendezvous(self.port_address, self.backlog)
         self.cListener.addConnection(self.tcpSocket)
 
-
-
     def initialize(self, angle, ready):
 
-       
         ShowBase.__init__(self)
         self.cTrav = CollisionTraverser()
         self.pusher = CollisionHandlerPusher()
@@ -60,7 +59,6 @@ class MyApp(ShowBase):
         dlnp = render.attachNewNode(dlight)
         render.setLight(dlnp)
 
-        
         self.screenWidth = base.win.getProperties().getXSize()
         self.screenHeight = base.win.getProperties().getYSize()
 
@@ -69,12 +67,12 @@ class MyApp(ShowBase):
         self.scoreText.setText(str(self.score))
         self.scoreTextPath = aspect2d.attachNewNode(self.scoreText)
         self.scoreTextPath.setScale(0.07)
-        self.scoreTextPath.setPos(1,0,0.9)
+        self.scoreTextPath.setPos(1, 0, 0.9)
 
-        base.setBackgroundColor(r=136/255,g=210/255,b=235/255)
+        base.setBackgroundColor(r=136 / 255, g=210 / 255, b=235 / 255)
         self.scene = self.loader.loadModel("models/environment")
         self.scene.reparentTo(self.render)
-        self.scene.setScale(200,200,1)
+        self.scene.setScale(200, 200, 1)
         self.scene.setPos(0, 0, -100)
 
         self.taskMgr.add(self.getAngle, "GetAngle")
@@ -84,8 +82,8 @@ class MyApp(ShowBase):
         planePos = self.plane.actor.getPos()
 
         base.disableMouse()
-        base.camera.setPos(0, planePos[1]+self.camZ, planePos[2]+self.camY)
-        base.camera.setHpr(0,self.camAngle,0)
+        base.camera.setPos(0, planePos[1] + self.camZ, planePos[2] + self.camY)
+        base.camera.setHpr(0, self.camAngle, 0)
 
         self.generateObstacles()
         self.taskMgr.add(self.checkHoops, "checkHoops")
@@ -99,21 +97,16 @@ class MyApp(ShowBase):
         planeCollider.setPythonTag("planeC", self.plane)
         # planeCollider.show()
 
-
-
         base.pusher.addCollider(planeCollider, self.plane.actor)
         base.cTrav.addCollider(planeCollider, self.pusher)
 
         self.accept("planeCollider-into-hoopCollider", self.handleCollision)
 
-
         self.taskMgr.add(self.movePlane, "movePlane")
         self.taskMgr.add(self.animateHoops, "animateHoops")
 
-
         self.taskMgr.add(self.tskListenerPolling, "Poll the connection listener", -39)
         self.taskMgr.add(self.tskReaderPolling, "Poll the connection reader", -40)
-
 
     def tskListenerPolling(self, taskdata):
         if self.cListener.newConnectionAvailable():
@@ -125,34 +118,39 @@ class MyApp(ShowBase):
 
             if self.cListener.getNewConnection(rendezvous, netAddress, newConnection):
                 newConnection = newConnection.p()
-                self.activeConnections.append(newConnection) # Remember connection
-                self.cReader.addConnection(newConnection)     # Begin reading connection
+                self.activeConnections.append(newConnection)  # Remember connection
+                self.cReader.addConnection(newConnection)  # Begin reading connection
         return Task.cont
 
     def tskReaderPolling(self, taskdata):
         if self.cReader.dataAvailable():
             datagram = NetDatagram()
             if self.cReader.getData(datagram):
-                print("get data")
-                print(datagram)
+                # print("get data")
+                # print(datagram)
                 self.incoming(datagram)
 
         return Task.cont
 
     def incoming(self, datagram):
         # print(datagram)
+
         iterator = PyDatagramIterator(datagram)
-        pressure = iterator.getString().replace(",",".")
+        pressure = iterator.getString().replace(",", ".")
+        # print(pressure)
         xy = pressure.split(";")
         # print(pressure)
         # print(xy)
         # xy = re.sub(r'[^\x00-\x7F]+','-', xy)
-        pressurex = float(xy[0])
-        pressurey = float(xy[1])
-        print(pressurex,pressurey)
+        pressurex = float(xy[0]) - self.baselineX
+        pressurey = float(xy[1]) - self.baselineY
+        if not self.collecting:
+            self.collecting = True
+            self.baselineX = pressurex
+            self.baselineY = pressurey
+        print(pressurex, pressurey)
 
-
-        if pressurex<0:
+        if pressurex < 0:
             print("moving left")
             self.plane.stopMovingRight()
             self.plane.moveLeft()
@@ -160,7 +158,7 @@ class MyApp(ShowBase):
             print("moving right")
             self.plane.stopMovingLeft()
             self.plane.moveRight()
-        if pressurey<0:
+        if pressurey < 0:
             print("moving down")
             self.plane.stopMovingUp()
             self.plane.moveDown()
@@ -169,9 +167,8 @@ class MyApp(ShowBase):
             self.plane.stopMovingDown()
             self.plane.moveUp()
 
-
     def checkHoops(self, task):
-        #get hoop Z and compare to plane Z
+        # get hoop Z and compare to plane Z
         # print(self.hoops[0].getPos()[1], self.plane.actor.getPos()[1])
         if self.hoops[0].getPos()[1] < self.plane.actor.getPos()[1]:
             self.hoops.pop(0).delete()
@@ -191,7 +188,6 @@ class MyApp(ShowBase):
         collider.clearPythonTag("hoop")
         hoop.delete()
         self.hoops.pop(0)
-        
 
     def movePlane(self, task):
 
@@ -215,31 +211,29 @@ class MyApp(ShowBase):
 
         # print(newX, newY)
 
-        self.plane.actor.setPos(newX, planePos[1]+self.plane.speed, newY)
+        self.plane.actor.setPos(newX, planePos[1] + self.plane.speed, newY)
 
-        if self.plane.leftMove !=0 or self.plane.rightMove!=0:
+        if self.plane.leftMove != 0 or self.plane.rightMove != 0:
             self.plane.rotatePlaneHorizontal(planeHpr)
         else:
             self.plane.recoverRotationHorizontal(planeHpr)
 
         planeHpr = self.plane.actor.getHpr()
 
-        if self.plane.upMove!=0 or self.plane.downMove!=0:
+        if self.plane.upMove != 0 or self.plane.downMove != 0:
             self.plane.rotatePlaneVertical(planeHpr)
         else:
             self.plane.recoverRotationVertical(planeHpr)
 
-        
-        base.camera.setPos(planePos[0]+self.camX, planePos[1]+self.camZ, planePos[2]+self.camY)
+        base.camera.setPos(planePos[0] + self.camX, planePos[1] + self.camZ, planePos[2] + self.camY)
         return Task.cont
-
 
     def generateObstacles(self):
         for i in range(self.numObstacles):
             hoop = Actor("assets/target.gltf")
-            hoop.setScale(50,50,50)
-            hoop.setPos(random.randint(-300,300),-700+(i*self.hoopGap),random.randint(80, 300))
-            hoop.setHpr(0,90,0)
+            hoop.setScale(50, 50, 50)
+            hoop.setPos(random.randint(-300, 300), -700 + (i * self.hoopGap), random.randint(80, 300))
+            hoop.setHpr(0, 90, 0)
             # hoop.setColor(1,0,0,1)
             hoop.reparentTo(self.render)
             self.hoops.append(hoop)
@@ -253,9 +247,9 @@ class MyApp(ShowBase):
     def getAngle(self, task):
         # print(self.angle.value)
         if self.angle.value > 0:
-            self.plane.setPos(self.plane, 1,0,0)
+            self.plane.setPos(self.plane, 1, 0, 0)
         elif self.angle.value < 0:
-            self.plane.setPos(self.plane, -1,0,0)
+            self.plane.setPos(self.plane, -1, 0, 0)
         return Task.cont
 
     def go(self, angle, ready):
